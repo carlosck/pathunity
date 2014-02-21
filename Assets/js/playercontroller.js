005956a28dfea9a85dd43f6c84c3e56c01bf5ab4
carlosck <carlosck@gmail.com>
2014-02-21 12:37:13 -0600

    cambio

---
+++ Assets/js/playercontroller.js
@@ -0,0 +1,17 @@
+﻿#pragma strict
+
+function Start () {
+	var cupoer : Collider[] =  GameObject.FindGameObjectWithTag("cuerpo").GetComponents.<Collider>();
+	var cols : Collider[] = GameObject.FindGameObjectWithTag("Player").GetComponents.<Collider>();
+    Physics.IgnoreCollision(cols[0], cupoer[0]);
+    
+    for (var C : Collider in cols)
+    {
+        Physics.IgnoreCollision(C, collider);
+    }
+
+}
+
+function Update () {
+
+}
